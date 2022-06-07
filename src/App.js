26d7757a028d55817a187533a3073efd1179e114
Susan Choi <2539lee@gmail.com>
2022-06-07 11:34:03 -0700

    Setup and Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Button, Navbar, Container, Nav } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar bg='light' variant='light'>
+        <Container>
+          <Navbar.Brand href='#home'>ShoeShop</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#features'>Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg'></div>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <img
+              src='https://codingapple1.github.io/shop/shoes1.jpg'
+              width='80%'
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className='col-md-4'>
+            <img
+              src='https://codingapple1.github.io/shop/shoes2.jpg'
+              width='80%'
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className='col-md-4'>
+            <img
+              src='https://codingapple1.github.io/shop/shoes3.jpg'
+              width='80%'
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
