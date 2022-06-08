477bc27be6e3007eb20ec9b390bc3fbe6e3829fa
Susan Choi <2539lee@gmail.com>
2022-06-07 23:35:24 -0700

    Card Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css'
 import { Button, Navbar, Container, Nav } from 'react-bootstrap'
+import data from './data'
+import { useState } from 'react'
 
 function App() {
+  let [shoes] = useState(data)
+
   return (
     <div className='App'>
       <Navbar bg='light' variant='light'>
@@ -18,32 +22,25 @@ function App() {
 
       <div className='container'>
         <div className='row'>
-          <div className='col-md-4'>
-            <img
-              src='https://codingapple1.github.io/shop/shoes1.jpg'
-              width='80%'
-            />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className='col-md-4'>
-            <img
-              src='https://codingapple1.github.io/shop/shoes2.jpg'
-              width='80%'
-            />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className='col-md-4'>
-            <img
-              src='https://codingapple1.github.io/shop/shoes3.jpg'
-              width='80%'
-            />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
+          <Card shoes={shoes[0]} i={1} />
+          <Card shoes={shoes[1]} i={2} />
+          <Card shoes={shoes[2]} i={3} />
         </div>
       </div>
+    </div>
+  )
+}
+
+function Card(props) {
+  return (
+    <div className='col-md-4'>
+      <img
+        src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'}
+        width='80%'
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
     </div>
   )
 }
