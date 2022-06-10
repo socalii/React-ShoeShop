edf224db127ec4be658e36c3ae55ab17a21fe141
Susan Choi <2539lee@gmail.com>
2022-06-10 00:36:17 -0700

    useEffect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,28 +1,54 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
+import { useEffect, useState } from 'react'
 
-const Box = styled.div`
-  background: grey;
-  padding: 20px;
-`
+// const Box = styled.div`
+//   background: grey;
+//   padding: 20px;
+// `
 
-const Btn = styled.button`
-  background: ${(props) => props.bg};
-  color: ${(props) => (props.bg === 'blue' ? 'white' : 'black')};
-  padding: 10px;
-`
+// const Btn = styled.button`
+//   background: ${(props) => props.bg};
+//   color: ${(props) => (props.bg === 'blue' ? 'white' : 'black')};
+//   padding: 10px;
+// `
 
 function Detail(props) {
   let { id } = useParams()
   let picId = parseInt(id) + 1
   let foundItem = props.shoes.find((x) => x.id == id)
 
+  // const [alert, setAlert] = useState(true)
+  const [num, setNum] = useState('')
+
+  useEffect(() => {
+    // setTimeout(() => {
+    //   setAlert(false)
+    // }, 2000)
+
+    if (isNaN(num) === true) {
+      alert('Not a number')
+    }
+  }, [num])
+
   return (
     <div className='container'>
-      <Btn bg='blue'>Button</Btn>
+      {/* <Btn bg='blue'>Button</Btn>
       <Btn bg='orange'>Button</Btn>
-      <Btn bg='yellow'>Button</Btn>
+      <Btn bg='yellow'>Button</Btn> */}
+
+      {/* {alert === true ? (
+        <div className='alert alert-warning'>
+          Get coupon code once bought in 2 secs
+        </div>
+      ) : null} */}
+
+      <input
+        onChange={(e) => {
+          setNum(e.target.value)
+        }}
+      />
 
       <div className='row'>
         <div className='col-md-6'>
